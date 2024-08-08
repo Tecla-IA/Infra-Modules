5bb9e3a288ad249fc3e2c8f80beff0a9b697616b
Santiago Reyes <sreyes@itba.edu.ar>
2024-08-08 12:31:57 -0300

    added sensitive

--- modules/storage-account/outputs.tf
+++ modules/storage-account/outputs.tf
@@ -5,5 +5,6 @@ output "storage_account_name" {
 
 output "storage_account_key" {
   description = "Returns the storage account's key"
+  sensitive   = true
   value       = azurerm_storage_account.account.primary_access_key
 }
